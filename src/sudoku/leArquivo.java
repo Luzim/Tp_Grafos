fd612eb8c94f69d0a3ef693a9ac287c7bdda7bfc
igororamos <igororamos@gmail.com>
2017-06-22 23:16:55 -0300

    leitura de arquivo

---
+++ src/sudoku/leArquivo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpgrafos;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Igor
+ */
+public class leArquivo {
+    public static String leitor(String path) throws IOException {
+        BufferedReader buffRead = new BufferedReader(new FileReader(path));
+        String linha = "";
+        while (true) {
+            if (linha != null) {
+                System.out.println(linha);
+ 
+            } else
+                break;
+            //linha = buffRead.readLine();
+        }
+        buffRead.close();
+        return linha;
+    }
+}
